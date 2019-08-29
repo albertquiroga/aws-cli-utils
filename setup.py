b1b120c055f4d16d5ab3ec866a5ca9aa23c2490b
Albert Quiroga <albertquirogabertolin@gmail.com>
2019-08-29 11:53:39 +0100

    Crawl refactor

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='bertolb-tools',
     version='0.0.1',
-    packages=['crawl', 'myutils'],
+    packages=['crawl', 'bertolb_utils'],
     entry_points={
         'console_scripts': [
-            'crawl=crawl.__main__:run'
+            'crawl=crawl.__init__:run'
         ]
     },
     install_requires=['boto3']
