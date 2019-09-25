a8789198b94dad42c715e2f200358cf24c931b98
Albert Quiroga <albertquirogabertolin@gmail.com>
2019-09-25 16:36:21 +0100

    Added tunnel option to devendpoint Improved _combine_ssh_args to remove unnecessary spaces in the string

--- bertolb_utils/__init__.py
+++ bertolb_utils/__init__.py
@@ -6,7 +6,9 @@ def format_location(raw_location):
 
 
 def _combine_ssh_args(hostname, username, key_path=None, port=22, ssh_options='', extra_args=''):
-    return "ssh %s%s %s@%s -p %i %s"%(ssh_options, " -i " + key_path if key_path else '', username, hostname, port, extra_args)
+    import re
+    command = "ssh %s %s %s@%s -p %i %s"%(ssh_options, "-i " + key_path if key_path else '', username, hostname, port, extra_args)
+    return re.sub(' +', ' ', command.strip())  # Remove leading/trailing spaces plus double spaces in the middle
 
 
 def build_ssh_command(hostname, username, pkey_path, port=22, ssh_options='', extra_args='',
@@ -15,7 +17,7 @@ def build_ssh_command(hostname, username, pkey_path, port=22, ssh_options='', ex
     res = _combine_ssh_args(hostname, username, pkey_path, port, ssh_options, extra_args)
 
     if bastion:
-        return _combine_ssh_args(bastion_hostname, bastion_username, bastion_pkey_path, bastion_port, ssh_options='-At -o StrictHostKeyChecking=no -o ServerAliveInterval=10') + ' ' + res
+        return _combine_ssh_args(bastion_hostname, bastion_username, bastion_pkey_path, bastion_port, ssh_options='-At -o StrictHostKeyChecking=no -o ServerAliveInterval=10', extra_args=res)
     elif tunnel:
         return res + ' -ND ' + str(tunnel_port)
     else:
--- glue/devendpoint/devendpoint_driver.py
+++ glue/devendpoint/devendpoint_driver.py
@@ -6,12 +6,14 @@ from bertolb_utils import build_ssh_command
 DEVENDPOINT_DEFAULT_NAME = 'bertolb-s3'
 DEFAULT_USERNAME = 'glue'
 PKEY_DEFAULT_PATH = '/Users/bertolb/aws/keypairs/glue/glue'
+TUNNEL_ARGS = '-vnNT -L :9007:169.254.76.1:9007'
 
 glue = boto3.client('glue')
 
 parser = argparse.ArgumentParser(prog='devendpoint', description='Connect to Glue dev endpoints')
 parser.add_argument('-n', '--name', action='store', default=DEVENDPOINT_DEFAULT_NAME, help='name of the dev endpoint to connect to')
 parser.add_argument('-k', '--key', action='store', default=PKEY_DEFAULT_PATH, help='path to private SSH key to use')
+parser.add_argument('-t', '--tunnel', action='store_true', default=False, help='create an SSH tunnel instead of REPL')
 parser.add_argument('-p', '--print', action='store_true', default=False, help='print SSH command instead of running it')
 parser.add_argument('-l', '--language', action='store', default='python', choices=['python', 'scala', 'none'], help='what REPL to launch (or none). Default: python')
 
@@ -33,5 +35,5 @@ def main():
     command = build_ssh_command(hostname=hostname,
                                 username=DEFAULT_USERNAME,
                                 pkey_path=cli_args_dict['key'],
-                                extra_args=language_dict[cli_args_dict['language']])
+                                extra_args=TUNNEL_ARGS if cli_args_dict['tunnel'] else language_dict[cli_args_dict['language']])
     print(command) if cli_args_dict['print'] else os.system(command)
