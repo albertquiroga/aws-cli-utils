3cbc7dc523419025c55ebcf114f253e79d6b4323
Albert Quiroga <albertquirogabertolin@gmail.com>
2020-02-28 14:21:44 +0000

    Added descriptions to all comamnds in pscase

--- pscase/pscase.py
+++ pscase/pscase.py
@@ -6,11 +6,11 @@ from pscase.utils import last_case_id, date_type
 from datetime import date
 
 # Top-level parser
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog='pscase', description='Manage AWS PS cases')
 subparsers = parser.add_subparsers()
 
 # New command
-parser_new = subparsers.add_parser('new')
+parser_new = subparsers.add_parser('new', description='Create a new AWS PS case')
 parser_new.add_argument('--number', action='store', default=str(last_case_id + 1), help='Local case number')
 parser_new.add_argument('--date', action='store', type=date_type, default=date.today().strftime("%m/%d/%y"),
                         help='Date in mm/dd/yy format')
@@ -23,7 +23,7 @@ parser_new.add_argument('--test', action='store_true', default=False, help='Writ
 parser_new.set_defaults(func=create_new_case)
 
 # Open command
-parser_open = subparsers.add_parser('open')
+parser_open = subparsers.add_parser('open', description='Open an AWS PS case')
 parser_open_group = parser_open.add_mutually_exclusive_group(required=True)
 parser_open_group.add_argument('-i', '--id', action='store', type=int, help='Open a case file with its Paragon case ID')
 parser_open_group.add_argument('-n', '--number', action='store', type=int,
@@ -31,7 +31,7 @@ parser_open_group.add_argument('-n', '--number', action='store', type=int,
 parser_open.set_defaults(func=open_case)
 
 # Bulkload command
-parser_bulkload = subparsers.add_parser('bulkload')
+parser_bulkload = subparsers.add_parser('bulkload', description='Loads a text file with case data into the DDB table')
 parser_bulkload.add_argument('file', help='TSV file to upload')
 parser_bulkload.add_argument('--test', action='store_true', default=False, help='Load to test DDB table instead')
 parser_bulkload.set_defaults(func=bulk_load)
