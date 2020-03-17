9bc1877cdac74be4a472e00b103d2d4adf3b8ce9
Albert Quiroga <albertquirogabertolin@gmail.com>
2020-03-17 13:02:30 +0000

    Fixed all dependency issues after the split
    Fixed dependencies and reference across both projects
     - Fixed the setup.py file for both projects
     - Added a new 'main' package to the pscase project, since it seems setuptools can't handle a root main file
     - Fixed all import statements across all files in pscase

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bertolb-tools',
+    name='aws-cli-utils',
     version='0.0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'glue=glue.__main__:main',
             'ec2=ec2.__main__:main',
-            'emr=emr.__main__:main',
-            'pscase=pscase.__main__:main'
+            'emr=emr.__main__:main'
         ]
     },
-    install_requires=['boto3', 'pyperclip', 'botocore']
+    install_requires=['boto3', 'botocore']
 )
