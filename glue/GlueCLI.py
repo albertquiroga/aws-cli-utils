28d2b41e8d2a21132b394a0e8ff492f1c42ee2d1
Albert Quiroga <albertquirogabertolin@gmail.com>
2020-03-23 18:38:58 +0000

    Reworked Glue command to now use the inheritance model
        This commit's changes:
         - The Glue command is now implemented using the inheritance structure
         - The Glue command now supports providing default values through the user configuration file. Users can configure default values for 'DefaultDevEndpointName' and 'DefaultNotebookName'

--- glue/GlueCLI.py
+++ glue/GlueCLI.py
@@ -7,45 +7,25 @@ from glue.notebook import connect_to_notebook
 class GlueCLI(CLITool):
 
     def __init__(self):
-        super(EC2CLI, self).__init__(name='glue', description='CLI tool to manage Glue resources', config_key='Glue',
-                                     key_parameters=['name'])
+        super(GlueCLI, self).__init__(name='glue', description='CLI tool to manage Glue resources', config_key='Glue',
+                                      key_parameters={'connect': ['name'], 'crawl': ['path'], 'notebook': ['name']})
 
-# Top-level parser
-parser = argparse.ArgumentParser(prog='glue', description='CLI tool to manage Glue resources')
-subparsers = parser.add_subparsers()
+        # Connect command
+        parser_connect = self.subparsers.add_parser('connect', description='Connect to Glue development endpoints')
+        parser_connect.add_argument('name', type=str, nargs='?', default=self.config.get('DefaultDevEndpointName', ''),
+                                    help='Name of the dev endpoint to connect to')
+        parser_connect.add_argument('-k', '--key', type=str, help='Path to private key file to use for auth')
+        parser_connect.add_argument('-p', '--print', action='store_true', default=False,
+                                    help='print the SSH command instead of executing it')
+        parser_connect.set_defaults(func=connect_to_dev_endpoint)
 
-# Connect command
-parser_connect = subparsers.add_parser('connect', description='Connect to Glue development endpoints')
-parser_connect.add_argument('name', type=str, nargs='?', default=DEFAULT_DEV_ENDPOINT_NAME,
-                            help='Name of the dev endpoint to connect to')
-parser_connect.add_argument('-k', '--key', type=str, help='Path to private key file to use for auth')
-parser_connect.add_argument('-p', '--print', action='store_true', default=False,
-                            help='print the SSH command instead of executing it')
-parser_connect.set_defaults(func=connect_to_dev_endpoint)
+        # Crawl command
+        parser_crawl = self.subparsers.add_parser('crawl', description='Run a crawler')
+        parser_crawl.add_argument('path', type=str, help='s3 path to be crawled')
+        parser_crawl.set_defaults(func=crawl)
 
-# Crawl command
-parser_crawl = subparsers.add_parser('crawl', description='Run a crawler')
-parser_crawl.add_argument('path', type=str, help='s3 path to be crawled')
-parser_crawl.set_defaults(func=crawl)
-
-# Notebook command
-parser_notebook = subparsers.add_parser('notebook', description='Connect to a Notebook')
-parser_notebook.add_argument('name', type=str, help='Name of the notebook to connect to. Partial match allowed!')
-parser_notebook.set_defaults(func=connect_to_notebook)
-
-
-def main():
-    """
-    Main function. Evaluates the CLI args and prints the help message if there are none
-    :return: None
-    """
-    args = parser.parse_args()
-
-    if not len(vars(args)) == 0:
-        args.func(args)
-    else:
-        parser.print_help()
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+        # Notebook command
+        parser_notebook = self.subparsers.add_parser('notebook', description='Connect to a Notebook')
+        parser_notebook.add_argument('name', type=str, nargs='?', default=self.config.get('DefaultNotebookName', ''),
+                                     help='Name of the notebook to connect to. Partial match allowed!')
+        parser_notebook.set_defaults(func=connect_to_notebook)
